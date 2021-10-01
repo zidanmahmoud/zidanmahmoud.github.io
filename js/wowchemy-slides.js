d41c519889ff903fccab1c861ab84f4b118b584b
Mahmoud <m.zidan@tum.de>
2021-10-01 18:56:52 +0200

    update hugo and CV

--- js/wowchemy-slides.js
+++ js/wowchemy-slides.js
@@ -1,6 +1,6 @@
 (() => {
   // ns-params:@params
-  var slides = {highlight_style: "dracula", theme: "black"};
+  var slides = { highlight_style: "dracula", theme: "black" };
 
   // ns-hugo:/tmp/hugo_cache/modules/filecache/modules/pkg/mod/github.com/wowchemy/wowchemy-hugo-modules/wowchemy/v5@v5.0.0-20210629192904-559885af86b7/assets/js/wowchemy-utils.js
   function fixMermaid(render = false) {
